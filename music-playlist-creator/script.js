01572d69b4bdcfd5f90a24ffa1c77911e0e62048
Victoria Hernandez <vlhernan@meta.com>
2025-06-04 16:38:11 -0700

    checkpoint 1 and 2 working fully, UI done

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,12 @@
+// make the modal appear when a card is clicked
+document.querySelectorAll('.card').forEach(card => {
+    card.addEventListener('click', () => {
+        const modal = document.querySelector('.modal-overlay').style.display = 'block';
 
+    });
+ });
+
+// close the modal when the close button is clicked
+ document.querySelector('.close').addEventListener('click', () => {
+    document.querySelector('.modal-overlay').style.display = 'none';
+ });
