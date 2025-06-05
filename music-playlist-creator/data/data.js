dc74d1dccdcc06150fba65f7805199115bdf0964
Victoria Hernandez <vlhernan@meta.com>
2025-06-04 22:04:50 -0700

    finished part 3 and 4

---
+++ music-playlist-creator/data/data.js
@@ -0,0 +1,345 @@
+let playlists = [
+    {
+        "playlistID": "1",
+        "playlist_name": "Workout",
+        "playlist_author": "V. Hernandez",
+        "playlist_art": undefined,
+        "likes": 10,
+        "songs": [
+            {
+                "songID": "1",
+                "song_name": "Work Out",
+                "song_artist": "J. Cole",
+                "song_art": undefined,
+                "song_album": "Cole World: The Sideline Story",
+                "song_duration": "3:55"
+
+            },
+            {
+                "songID": "2",
+                "song_name": "Starboy",
+                "song_artist": "The Weeknd",
+                "song_art": undefined,
+                "song_album": "Starboy",
+                "song_duration": "2:50"
+            },
+            {
+                "songID": "3",
+                "song_name": "Swimming Pools",
+                "song_artist": "Kendrick Lamar",
+                "song_art": undefined,
+                "song_album": "good kid, m.A.A.d city (Deluxe)",
+                "song_duration": "5:12"
+            },
+            {
+                "songID": "4",
+                "song_name": "Savage",
+                "song_artist": "Megan Thee Stallion",
+                "song_art": undefined,
+                "song_album": "Suga",
+                "song_duration": "3:33"
+            }
+        ]
+    },
+
+    {
+        "playlistID": "2",
+        "playlist_name": "Art Deco",
+        "playlist_author": "Spotify",
+        "playlist_art": undefined,
+        "likes": 1,
+        "songs": [
+            {
+                "songID": "1",
+                "song_name": "West Coast",
+                "song_artist": "Lana Del Rey",
+                "song_art": undefined,
+                "song_album": "Ultraviolence",
+                "song_duration": "3:20"
+
+            },
+            {
+                "songID": "2",
+                "song_name": "Into You",
+                "song_artist": "Ariana Grande",
+                "song_art": undefined,
+                "song_album": "Dangerous Woman",
+                "song_duration": "2:50"
+            },
+            {
+                "songID": "3",
+                "song_name": "Dangerous Woman",
+                "song_artist": "Ariana Grande",
+                "song_art": undefined,
+                "song_album": "Dangerous Woman",
+                "song_duration": "4:20"
+            },
+            {
+                "songID": "4",
+                "song_name": "lacy",
+                "song_artist": "Olivia Rodrigo",
+                "song_art": undefined,
+                "song_album": "guts",
+                "song_duration": "2:12"
+            }
+        ]
+    },
+
+    {
+        "playlistID": "3",
+        "playlist_name": "Best of TSwift",
+        "playlist_author": "Vic",
+        "playlist_art": undefined,
+        "likes": 3,
+        "songs": [
+            {
+                "songID": "1",
+                "song_name": "Red",
+                "song_artist": "Taylor Swift",
+                "song_art": undefined,
+                "song_album": "Red",
+                "song_duration": "3:13"
+
+            },
+            {
+                "songID": "2",
+                "song_name": "Fearless",
+                "song_artist": "Taylor Swift",
+                "song_art": undefined,
+                "song_album": "Fearless",
+                "song_duration": "2:13"
+            },
+            {
+                "songID": "3",
+                "song_name": "Lover",
+                "song_artist": "Taylor Swift",
+                "song_art": undefined,
+                "song_album": "Lover",
+                "song_duration": "5:13"
+            },
+            {
+                "songID": "4",
+                "song_name": "Midnights",
+                "song_artist": "Taylor Swift",
+                "song_art": undefined,
+                "song_album": "Midnights",
+                "song_duration": "3:13"
+            }
+        ]
+    },
+
+    {
+        "playlistID": "4",
+        "playlist_name": "Lana Favs",
+        "playlist_author": "LDR Luvr",
+        "playlist_art": undefined,
+        "likes": 5,
+        "songs": [
+            {
+                "songID": "1",
+                "song_name": "Ultraviolence",
+                "song_artist": "Lana Del Rey",
+                "song_art": undefined,
+                "song_album": "Ultraviolence",
+                "song_duration": "3:55"
+
+            },
+            {
+                "songID": "2",
+                "song_name": "Blue Jeans",
+                "song_artist": "Lana Del Rey",
+                "song_art": undefined,
+                "song_album": "Born to Die",
+                "song_duration": "2:20"
+            },
+            {
+                "songID": "3",
+                "song_name": "Born to Die",
+                "song_artist": "Lana Del Rey",
+                "song_art": undefined,
+                "song_album": "Born to Die",
+                "song_duration": "4:30"
+            },
+            {
+                "songID": "4",
+                "song_name": "Money Power Glory",
+                "song_artist": "Lana Del Rey",
+                "song_art": undefined,
+                "song_album": "Ultraviolence",
+                "song_duration": "3:33"
+            }
+        ]
+    },
+
+    {
+        "playlistID": "5",
+        "playlist_name": "Workout (2)",
+        "playlist_author": "Victoria Hernandez",
+        "playlist_art": undefined,
+        "likes": 0,
+        "songs": [
+            {
+                "songID": "1",
+                "song_name": "Work Out",
+                "song_artist": "J. Cole",
+                "song_art": undefined,
+                "song_album": "Cole World: The Sideline Story",
+                "song_duration": "3:55"
+
+            },
+            {
+                "songID": "2",
+                "song_name": "Starboy",
+                "song_artist": "The Weeknd",
+                "song_art": undefined,
+                "song_album": "Starboy",
+                "song_duration": "2:50"
+            },
+            {
+                "songID": "3",
+                "song_name": "Swimming Pools",
+                "song_artist": "Kendrick Lamar",
+                "song_art": undefined,
+                "song_album": "good kid, m.A.A.d city (Deluxe)",
+                "song_duration": "5:12"
+            },
+            {
+                "songID": "4",
+                "song_name": "Savage",
+                "song_artist": "Megan Thee Stallion",
+                "song_art": undefined,
+                "song_album": "Suga",
+                "song_duration": "3:33"
+            }
+        ]
+    },
+
+    {
+        "playlistID": "6",
+        "playlist_name": "Workout (3)",
+        "playlist_author": "Victoria Hernandez",
+        "playlist_art": undefined,
+        "likes": 0,
+        "songs": [
+            {
+                "songID": "1",
+                "song_name": "Work Out",
+                "song_artist": "J. Cole",
+                "song_art": undefined,
+                "song_album": "Cole World: The Sideline Story",
+                "song_duration": "3:55"
+
+            },
+            {
+                "songID": "2",
+                "song_name": "Starboy",
+                "song_artist": "The Weeknd",
+                "song_art": undefined,
+                "song_album": "Starboy",
+                "song_duration": "2:50"
+            },
+            {
+                "songID": "3",
+                "song_name": "Swimming Pools",
+                "song_artist": "Kendrick Lamar",
+                "song_art": undefined,
+                "song_album": "good kid, m.A.A.d city (Deluxe)",
+                "song_duration": "5:12"
+            },
+            {
+                "songID": "4",
+                "song_name": "Savage",
+                "song_artist": "Megan Thee Stallion",
+                "song_art": undefined,
+                "song_album": "Suga",
+                "song_duration": "3:33"
+            }
+        ]
+    },
+
+    {
+        "playlistID": "7",
+        "playlist_name": "Workout (4)",
+        "playlist_author": "Victoria Hernandez",
+        "playlist_art": undefined,
+        "likes": 0,
+        "songs": [
+            {
+                "songID": "1",
+                "song_name": "Work Out",
+                "song_artist": "J. Cole",
+                "song_art": undefined,
+                "song_album": "Cole World: The Sideline Story",
+                "song_duration": "3:55"
+
+            },
+            {
+                "songID": "2",
+                "song_name": "Starboy",
+                "song_artist": "The Weeknd",
+                "song_art": undefined,
+                "song_album": "Starboy",
+                "song_duration": "2:50"
+            },
+            {
+                "songID": "3",
+                "song_name": "Swimming Pools",
+                "song_artist": "Kendrick Lamar",
+                "song_art": undefined,
+                "song_album": "good kid, m.A.A.d city (Deluxe)",
+                "song_duration": "5:12"
+            },
+            {
+                "songID": "4",
+                "song_name": "Savage",
+                "song_artist": "Megan Thee Stallion",
+                "song_art": undefined,
+                "song_album": "Suga",
+                "song_duration": "3:33"
+            }
+        ]
+    },
+
+    {
+        "playlistID": "8",
+        "playlist_name": "Workout (5)",
+        "playlist_author": "Victoria Hernandez",
+        "playlist_art": undefined,
+        "likes": 0,
+        "songs": [
+            {
+                "songID": "1",
+                "song_name": "Work Out",
+                "song_artist": "J. Cole",
+                "song_art": undefined,
+                "song_album": "Cole World: The Sideline Story",
+                "song_duration": "3:55"
+
+            },
+            {
+                "songID": "2",
+                "song_name": "Starboy",
+                "song_artist": "The Weeknd",
+                "song_art": undefined,
+                "song_album": "Starboy",
+                "song_duration": "2:50"
+            },
+            {
+                "songID": "3",
+                "song_name": "Swimming Pools",
+                "song_artist": "Kendrick Lamar",
+                "song_art": undefined,
+                "song_album": "good kid, m.A.A.d city (Deluxe)",
+                "song_duration": "5:12"
+            },
+            {
+                "songID": "4",
+                "song_name": "Savage",
+                "song_artist": "Megan Thee Stallion",
+                "song_art": undefined,
+                "song_album": "Suga",
+                "song_duration": "3:33"
+            }
+        ]
+    }
+]
--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,12 +1,149 @@
-// make the modal appear when a card is clicked
-document.querySelectorAll('.card').forEach(card => {
-    card.addEventListener('click', () => {
-        const modal = document.querySelector('.modal-overlay').style.display = 'block';
+// =========== DYNAMICALLY ADDING THE MODAL SONG ELEMENTS ============
 
+let num_playlists = 8;
+
+// takes in one song object and returns the html code for the song
+const createSongElement = (song) => {
+    // fetching elements of the song
+    let id = song.songID;
+    let name = song.song_name;
+    let artist = song.song_artist;
+    let album = song.song_album;
+    let duration = song.song_duration;
+
+
+    // turning into html
+    let middle = `
+                    <section class="song">
+                        <img src="assets/img/song.png" class="modal-song-img" alt="snoopy and woodstock in sunglasses">
+                        <section class="modal-song-subheader">
+                            <h3 class="modal-song-name">${name}</h3>
+                            <p class="song-info">${artist}</p>
+                            <p class="song-info">${album}</p>
+                            <p class="song-info">${duration}</p>
+                        </section>
+                    </section>`;
+
+    return middle;
+};
+
+const loadSongs = (songs, playlist) => {
+
+    // start of modal html code
+    let html = ` <div class="modal-content">
+    <span class="close" id="close${playlist.playlistID}">&times;</span>
+
+
+    <section class="modal-header">
+        <img src="assets/img/playlist.png" class="modal-playlist-img" alt="snoopy and woodstock in sunglasses">
+        <section class="modal-subheader">
+            <h2 class="modal-playlist-name">${playlist.playlist_name}</h2>
+            <h4 class="modal-playlist-creator">Created By ${playlist.playlist_author}</h4>
+        </section>
+    </section>
+
+    <section class="modal-body">`;
+
+    for(const song of songs){
+        html += createSongElement(song); // append song code
+    }
+
+    html += `</section>
+            </div>`; // end of modal html code
+
+    // create the modal element using the html code and append it to the body
+    const modalElement = document.createElement("section");
+    modalElement.className = "modal-overlay";
+    modalElement.id = "modal" + playlist.playlistID;
+    modalElement.innerHTML = html;
+
+    const body = document.querySelector("body");
+    body.append(modalElement);
+};
+
+
+// ======= ADDING PLAYLIST ELEMENTS =======
+
+//given a playlist JSON, put edit the default playlists to be this playlist
+const insertPlaylistElement = (playlist) => {
+    let playlistID = playlist.playlistID;
+    let playlist_name = playlist.playlist_name;
+    let playlist_author = playlist.playlist_author;
+    let playlist_likes = playlist.likes;
+
+
+    // set title/name
+    const title_element = document.getElementById("playlist-title" + playlistID); // would end up being playlist-title1 hopefully
+    title_element.textContent = playlist_name;
+
+    //set author
+    const author_element = document.getElementById("playlist-author" + playlistID); // would end up being playlist-author1 hopefully
+    author_element.textContent = playlist_author;
+
+    //set likes
+    const likes_element = document.getElementById("like-count" + playlistID); // would end up being like-count1 hopefully
+    likes_element.textContent = playlist_likes;
+
+    console.log(playlist);
+    loadSongs(playlist.songs, playlist); // next load the songs
+};
+
+
+ // parse through the playlists hashmap to populate the playlist cards
+ const loadPlaylists = () => {
+    console.log("loadPlaylists called");
+
+    if(!playlists){
+        console.log("No playlists to display");
+
+        // Display error message on a popup modal
+        const errorElement = document.createElement("div");
+        errorElement.innerHTML  = `
+        <section class="modal-overlay">
+            <div class="modal-content">
+                <h1>No Playlists to Display :( <h1>
+            </div>
+        </section>
+        `
+
+        const body = document.querySelector("body");
+        body.append(errorElement);
+    }
+    else{
+        // iterate through the playlists and add each one to the elements
+        for(const playlist of playlists){
+            console.log(playlist);
+            insertPlaylistElement(playlist);
+        }
+    }
+
+};
+
+// call loadPlaylists
+loadPlaylists();
+
+
+
+// =========== DISPLAYING/CLOSING THE MODAL ============
+
+// open appropriate modal when clicking the card
+ for (let i = 1; i <= num_playlists; i++) {
+    document.querySelector('#card' + i).addEventListener('click', () => {
+        const modal = document.querySelector("#modal" + i).style.display = 'block';
     });
- });
 
-// close the modal when the close button is clicked
- document.querySelector('.close').addEventListener('click', () => {
-    document.querySelector('.modal-overlay').style.display = 'none';
- });
+ }
+
+// close on clicking the close button
+ for (let i = 1; i <= num_playlists; i++) {
+    document.querySelector('#close' + i).addEventListener('click', () => {
+        document.querySelector('#modal' + i).style.display = 'none';
+    });}
+
+// close on clicking anywhere outside the modal
+for (let i = 1; i <= num_playlists; i++) {
+    document.querySelector('#modal' + i).addEventListener('click', () => {
+        document.querySelector('#modal' + i).style.display = 'none';
+    });
+
+ }
